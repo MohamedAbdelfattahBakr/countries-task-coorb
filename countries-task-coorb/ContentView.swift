a14331f14fe2346f0ab249c2bb2b68a1cced1761
Mohamed Bakr <mohbakr9@gmail.com>
2025-09-25 18:57:21 +0300

    Initial Commit

---
+++ countries-task-coorb/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  countries-task-coorb
+//
+//  Created by Mohamed Bakr on 25/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ countries-task-coorb/countries_task_coorbApp.swift
@@ -0,0 +1,17 @@
+//
+//  countries_task_coorbApp.swift
+//  countries-task-coorb
+//
+//  Created by Mohamed Bakr on 25/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct countries_task_coorbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
